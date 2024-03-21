fdac4b0a6a9f6d6511c63681312fa372f8150e38
Ojani Figueroa <ojanifigueroa.of@gmail.com>
2024-03-21 12:29:10 -0400

    Changed the structire of the calculator class to make calculations based on input parameters instead of using properties that were defined whene initializing a class instance. also refactored how trailing newline characters are handled for input files.

--- proyecto1.py
+++ proyecto1.py
@@ -1,76 +1,71 @@
 import sys
 
-# prints all the rows of a 2d array
-def printMatrix(matrix):
-    for row in matrix: print(row)  
+# prints all the rows of a 2d array (used for debugging)
+# def printMatrix(matrix):
+#     for row in matrix: print(row)  
 
 # Class for a calculator that generates an alignment matrix for the
 # input sequences and generates an alignment for them
 class AlignmentCalculator():
     # initializes all variables for the calculator
-    # generates a matrix with only the initial values.
-    def __init__(self, seq1, seq2, matchScore=1, mismatchScore=-1, gapPenalty=-2):
-        # setting up variables as well as width and height of alignment matrix
-        self.gapPenalty = gapPenalty
-        self.matchScore = matchScore
+    def __init__(self, matchScore, mismatchScore, gapPenalty):
         self.mismatchScore = mismatchScore
         self.gapPenalty = gapPenalty
-        self.width = len(seq1)+1
-        self.height = len(seq2)+1
-        self.seq1 = seq1
-        self.seq2 = seq2
+        self.matchScore = matchScore
 
-        # initializing alignment matrix
-        self.alignmentMatrix = [[0]*self.width for _ in range(self.height)]
+    # generates and returns an alignment matrix made from the input sequences
+    def generateAlignmentMatrix(self, seq1, seq2):
+        width = len(seq1)+1
+        height = len(seq2)+1
 
-        # setting initial values of matrix
-        for y in range(self.height): self.alignmentMatrix[y][0] = gapPenalty * y
-        for x in range(self.width): self.alignmentMatrix[0][x] = gapPenalty * x
+        # creating matrix with initial values
+        alignmentMatrix = [[0]*width for _ in range(height)]
 
-        # automatically filling matrix upon initializaion
-        self.fillMatrix()
+        for y in range(height): alignmentMatrix[y][0] = self.gapPenalty * y
+        for x in range(width): alignmentMatrix[0][x] = self.gapPenalty * x
 
-    # populates the alignment matrix
-    def fillMatrix(self):
-        for y in range(1,self.height):
-            for x in range(1,self.width):
-                # setting the score value to 1 if seq1[current] = seq2[current]
-                score = self.matchScore if self.seq1[x-1] == self.seq2[y-1] else self.mismatchScore
+        # calculating the score for each cell in the matrix
+        for y in range(1,height):
+            for x in range(1,width):
+                # setting the score value to either the match score or mismatch score
+                score = self.matchScore if seq1[x-1] == seq2[y-1] else self.mismatchScore
 
-                topLeftScore = self.alignmentMatrix[y-1][x-1]
-                leftScore = self.alignmentMatrix[y][x-1]
-                topScore = self.alignmentMatrix[y-1][x]
+                topLeftScore = alignmentMatrix[y-1][x-1]
+                leftScore = alignmentMatrix[y][x-1]
+                topScore = alignmentMatrix[y-1][x]
 
-                # deciding which value to put into the current cell
+                # putting the max calculated value into the current cell
                 value = max(topScore + self.gapPenalty, leftScore + self.gapPenalty, topLeftScore + score)
-                self.alignmentMatrix[y][x] = value
+                alignmentMatrix[y][x] = value
+
+        return alignmentMatrix
     
-    # returns a tuple containing two strings. seq1 aligned and seq2 aligned.
-    def getAlignments(self):
+    # returns a tuple containing two strings the alignment for seq1 and for seq2
+    def generateAlignments(self, seq1, seq2, matrix):
         alignedSeq1 = []
         alignedSeq2 = []
 
-        y = self.height - 1
-        x = self.width - 1
+        y = len(seq2)
+        x = len(seq1)
 
         # iterating backwards through the matrix matrix
         while x > 0 or y > 0:
-            currentScore = self.alignmentMatrix[y][x]
+            currentScore = matrix[y][x]
 
             # trying to see if the calculation using the left cell resulted in the max value
-            if self.alignmentMatrix[y][x-1] + self.gapPenalty == currentScore:
-                alignedSeq1.append(self.seq1[x-1])
-                alignedSeq2.append('-')
+            if matrix[y][x-1] + self.gapPenalty == currentScore:
+                alignedSeq1.append(seq1[x-1]) # seq1 appends the letter
+                alignedSeq2.append('-') # meanwhile for seq2, we add a gap
                 x -= 1
             # trying to see if the calculation using the top cell resulted in the max value
-            elif self.alignmentMatrix[y-1][x] + self.gapPenalty == currentScore:
-                alignedSeq2.append(self.seq2[y-1])
-                alignedSeq1.append('-')
+            elif matrix[y-1][x] + self.gapPenalty == currentScore:
+                alignedSeq2.append(seq2[y-1]) # seq2 appends the letter
+                alignedSeq1.append('-') # meanwhile for seq1, we add a gap
                 y -= 1
             # when there is no gap, just append the next letter in the sequence
             else:
-                alignedSeq1.append(self.seq1[x-1])
-                alignedSeq2.append(self.seq2[y-1])
+                alignedSeq1.append(seq1[x-1])
+                alignedSeq2.append(seq2[y-1])
                 x -= 1
                 y -= 1
         
@@ -80,9 +75,10 @@ class AlignmentCalculator():
         # actually copying all the characters over to a new string
         return (''.join(alignedSeq1[::-1]), ''.join(alignedSeq2[::-1]))
         
-    # returns the score of the matrix
-    def getScore(self): 
-        return self.alignmentMatrix[-1][-1]
+    # returns the score of the input matrix
+    @classmethod
+    def getScore(cls, matrix): 
+        return matrix[-1][-1]
 
 # returns a list of all the sequence pairs in tuples
 def getSequencePairs():
@@ -91,28 +87,31 @@ def getSequencePairs():
 
     with open(path) as csv:
         # using line breaks as separators in order to get a list of all the rows in the csv.
-        # the list is spliced from index 1 onwards in order to remove the csv titles/headers
-        pairs = csv.read().split('\n')[1:]
+        # the list is spliced from index 1 onwards in order to remove the csv titles/headers.
+        # rstrip is used to prevent errors caused by trailing newline characters
+        pairs = csv.read().rstrip().split('\n')[1:]
         
         # creating a tuple for each row which contains both
         # sequences by using the comma as the separator.
         return (pair.split(',') for pair in pairs) 
 
 def main():
+    MATCH_SCORE=1
+    MISMATCH_SCORE=-1
+    GAP_PENALTY=-2
+
     # getting list of sequence pairs from csv file
     sequencePairs = getSequencePairs()
     
     # computing and printing the alignment values for all the sequences
-    for pair in sequencePairs:
-        # Sometimes, extra line breaks cause issues because it is seen as
-        # another sequence pair, this fixes that by ignoring empty values.
-        if len(pair) < 2: continue
-        seq1, seq2 = pair
+    for seq1, seq2 in sequencePairs:
+        # initializing calculator with necessary parameters
+        calculator = AlignmentCalculator(MATCH_SCORE, MISMATCH_SCORE, GAP_PENALTY)
 
-        calculator = AlignmentCalculator(seq1, seq2)
+        alignmentMatrix = calculator.generateAlignmentMatrix(seq1, seq2)
+        alignedSeq1, alignedSeq2 = calculator.generateAlignments(seq1, seq2, alignmentMatrix)
 
-        alignedSeq1, alignedSeq2 = calculator.getAlignments()
-        print(alignedSeq1, alignedSeq2, calculator.getScore())
+        print(alignedSeq1, alignedSeq2, AlignmentCalculator.getScore(alignmentMatrix))
 
 if __name__ == "__main__":
     main()
